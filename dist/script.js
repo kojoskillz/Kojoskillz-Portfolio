5c3b4b7c4e3fbf3c0d999797fcfe79dd0e8c6e68
Kwadwo Nyarko <115382951+kojoskillz@users.noreply.github.com>
2023-11-22 04:13:59 +0000

    Add files via upload

---
+++ dist/script.js
@@ -0,0 +1,71 @@
+
+const typed = new Typed('#element', {
+    strings: ['Front-End Developer', 'Technical Writer'],
+    typeSpeed: 90,
+    backSpeed:20,
+    loop:true,  
+
+});
+
+
+
+// scroll_to_top
+    function scrollToTop() {
+        window.scrollTo(0,0);
+         
+    }
+
+
+
+// form validation
+document.getElementById("contactForm").addEventListener("submit", function(event) {
+    event.preventDefault(); // Prevent form submission
+   
+    // Get form values
+    var name = document.getElementById("name").value;
+    var email = document.getElementById("email").value;
+    var message = document.getElementById("message").value;
+   
+    // Perform form validation
+    if (name === "" || email === "" || message === "") {
+      alert("Please fill in all fields");
+      return;
+    }
+   
+    // Perform form submission
+    alert("Form shot successfully");
+   
+    // Clear form fields
+    document.getElementById("name").value = "";
+    document.getElementById("email").value = "";
+    document.getElementById("message").value = "";
+  });
+
+
+  
+  // loader
+let loader;
+function loadNow(opacity){
+  if(opacity <= 0) {
+    displayContent();
+    }
+    else {
+      loader.style.opacity = opacity;
+      window.setTimeout(function(){
+        loadNow(opacity - 0.5)
+      },100);
+    }
+  }
+
+  function displayContent() {
+    loader.style.display = 'none';
+    document.getElementById('content').style.display = 'block';
+  }
+
+  document.addEventListener("DOMContentLoaded",function() {
+    loader = document.getElementById('loader');
+    loadNow(1);
+  });
+
+
+
